66f93ffe41ba8d664aa667eabc3cff38b0f4cc4c
hoganbc <toobusyforthis@gmail.com>
2020-04-18 01:38:00 -0400

    Customize zsh prompt with p10k and aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,28 @@
+# Thanks to Oh-my-zsh contributors for colorizing ls
+# https://github.com/ohmyzsh/ohmyzsh/blob.master.kib/theme-and-appearance.zsh
+
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias grepsrc="find src -type f | xargs grep --line-number"
+alias greptest="find test -type f | xargs grep --line-number"
+
+alias lc='colorls --almost-all --long --sort-dirs'
+alias lg='colorls --git-status'
+if command -v colorls &> /dev/null; then
+    # prefer colorls
+    alias ls='colorls --git-status'
+    alias tree='ls --tree'
+    alias gls='gls --color=tty'
+elif command -v gls &> /dev/null; then
+    # prefer gnu ls
+    alias ls='gls --color=tty'
+    alias gls='gls --color=tty'
+else
+    # colorize plain ls
+    ls -G . &> /dev/null && alias ls='ls -G'
+fi
+alias lsa='ls -A'
+alias lsal='ls -Al'
+
---
+++ zsh/install.sh
@@ -0,0 +1,5 @@
+# colorls is installed via ruby
+
+if [[ ! -d ${HOME}/powerlevel10k ]]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${HOME}/powerlevel10k
+fi

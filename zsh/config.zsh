54c816f0322433ad0490a956cd5db085218a2de2
hoganbc <toobusyforthis@gmail.com>
2020-05-28 00:47:22 -0400

    Enable autopushd with zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -20,6 +20,7 @@ setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
+setopt AUTOPUSHD # pushd on cd
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
